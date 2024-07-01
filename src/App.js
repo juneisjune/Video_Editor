a4897b3be0cb5cf52b74084ec366d2d93f7d757e
juneisjune <yhs1535@naver.com>
2024-07-01 16:14:41 +0900

    불필요 파일 제거

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 import Accordion from 'react-bootstrap/Accordion';
-import EventByBootstrap from './pages/EventByBootstrap';
-import EventByAntd from './pages/EventByAntd';
+
+
 import VideoEditor from './pages/VideoEditor/VideoEditor'
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
